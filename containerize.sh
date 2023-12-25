5d760b34b767cae32c3023b7c7b8aa0d73e1fa49
Dennis Krasnov <dennis@krasnov.dev>
2023-12-25 14:26:49 -0500

    move CI to buildkite for security reasons

--- containerize.sh
+++ containerize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 CONTAINER=$(buildah from docker.io/library/nginx:1.25.3-alpine)
 buildah copy "$CONTAINER" nginx.conf /etc/nginx
 buildah copy "$CONTAINER" build /app
