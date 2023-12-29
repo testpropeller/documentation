093b04c80972df6dde56e3013e445f2a8f8c96dd
Dennis Krasnov <dennis@krasnov.dev>
2023-12-28 20:13:06 -0500

    clean up docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,132 +3,138 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Test Propeller',
-  tagline: 'Give each test its own prod-like environment in milliseconds.',
-  favicon: 'img/favicon.ico',
+    title: 'Test Propeller',
+    tagline: 'Give each test its own prod-like environment in milliseconds.',
+    favicon: 'img/favicon.ico',
 
-  url: 'https://testpropeller.com',
-  baseUrl: '/',
+    url: 'https://testpropeller.com',
+    baseUrl: '/',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Test Propeller',
-      logo: {
-        alt: 'Test Propeller logo',
-        src: 'img/logo.png',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Documentation',
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Test Propeller',
+            logo: {
+                alt: 'Test Propeller logo',
+                src: 'img/logo.png',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Documentation',
+                },
+                {
+                    to: '/blog',
+                    label: 'Blog',
+                    position: 'left',
+                },
+                {
+                    to: '/pricing',
+                    label: 'Pricing',
+                    position: 'left',
+                },
+                {
+                    href: '/console/sign-in',
+                    label: 'Sign in',
+                    position: 'right',
+                },
+            ],
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Resources',
+                    items: [
+                        {
+                            label: 'Documentation',
+                            to: '/docs',
+                        },
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/...',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/testpropeller',
+                        },
+                    ],
+                },
+                {
+                    title: 'Client Libraries',
+                    items: [
+                        {
+                            label: 'Java',
+                            to: '/docs/client-library/java',
+                        },
+                        {
+                            label: 'Rust',
+                            to: '/docs/client-library/rust',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Test Propeller`,
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Resources',
-          items: [
-            {
-              label: 'Documentation',
-              to: '/docs',
-            },
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/...',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/testpropeller',
-            },
-          ],
-        },
-        {
-          title: 'Client Libraries',
-          items: [
-            {
-              label: 'Rust',
-              to: '/docs/client-library/rust',
-            },
-            {
-              label: 'Java',
-              to: '/docs/client-library/java',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Test Propeller`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 
-  scripts: [
-    {
-      src: '/plausible/script.js',
-      defer: true,
-      'data-api': '/plausible/event',
-      'data-domain': 'testpropeller.com',
-    },
-  ],
+    scripts: [
+        {
+            src: '/plausible/script.js',
+            defer: true,
+            'data-api': '/plausible/event',
+            'data-domain': 'testpropeller.com',
+        },
+    ],
 };
 
 export default config;
